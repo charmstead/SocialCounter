2754a81cfbe9cf9593a4d6d262f32a136ab304eb
Tomide <tomideso@gmail.com>
2018-08-11 03:11:37 +0100

    modified

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var qs = require('qs');
 
-const url ='http://faceofzaron.zaron.com.ng/wp-admin/admin-ajax.php';
-const data = { 'action':'vote_for_photo','photo_id':'1323','nonce_id':'ebfea02ca1'};
+const url ='http://faceofzaron.zaron.com.ng/wp-admin/admin-ajax.php?jury=okay';
+const data = { 'action':'vote_for_photo','photo_id':'1323','nonce_id':'a60d83da5b'};
 const options = {
     method: 'POST',
     headers: {
@@ -9,8 +9,7 @@ const options = {
         'Referer': 'http://faceofzaron.zaron.com.ng/wp-admin/admin-ajax.php',
         'X-Requested-With': 'XMLHttpRequest',
         'User-Agent':"Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
-        Date: 'Thu, 09 Aug 2018 12:03:52 GMT'
-    },
+       },
     data: qs.stringify(data),
     url:url
 };
--- task/poll.js
+++ task/poll.js
@@ -3,61 +3,87 @@ var path = require('path');
 var qs = require('qs');
 const Proxy = require('../model/proxySchema')
 const options = require('../config')
+const proxies = require('./proxy')
 
 
 let counter = 0;
 
-let lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(path.join(__dirname, 'proxy.txt'))
-});
+// let lineReader = require('readline').createInterface({
+//     input: require('fs').createReadStream(path.join(__dirname, 'proxy.txt'))
+// });
 
-lineReader.on('line', function (line) {
-    counter++;
-    setTimeout(() => {
-        let proxy_address = line.split(":");
-        let proxy = { host: proxy_address[0], port: proxy_address[1] }
-        options.proxy=proxy;
-        console.log('proxy:', proxy);
-        console.log(counter)
-        axios(options)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(error => {
-                console.log("error");
-               
-            });
+// lineReader.on('line', function (line) {
+//     counter++;
+//     setTimeout(() => {
+//         let proxy_address = line.split(":");
+//         let proxy = { host: proxy_address[0], port: proxy_address[1] }
+//         options.proxy=proxy;
+//         console.log('proxy:', proxy);
+//         console.log(counter)
+//         axios(options)
+//             .then(res => {
+//                 console.log(res)
+//             })
+//             .catch(error => {
+//                 console.log("error");
 
-        
-    }, 2000 * counter);
+//             });
 
-});
 
-Proxy.find({ 'used': false },(err,prxy)=>{
-    
-    prxy.map((p,i)=>{
+//     }, 2000 * counter);
+
+// });
+
+
+// try {
+
+//     Proxy.insertMany(proxies, (err, result) => {
+
+//       console.log("Successfully added proxies")
+//     })
+//   }
+//   catch (e) {
+//      console.log("Error occurred")
+//   }
+
+
+
+Proxy.find({ 'used': false }, (err, prxy) => {
+
+    prxy.map((p, i) => {
 
         setTimeout(() => {
-
-            Proxy
-            .findByIdAndUpdate(p._id,
-                { 
-                    '$set': { used: true } }, (err, od) => {
-                })
+            console.log(i)
 
             let proxy_address = p.proxy.split(":");
             let proxy = { host: proxy_address[0], port: proxy_address[1] }
-            options.proxy=proxy;
+            options.proxy = proxy;
+            console.log(proxy)
             axios(options)
-            .then(res => {
-                console.log(res.statusCode)
-            })
-            .catch(error => {
-                console.log("error");
-               
-            });
+                .then(res => {
+                    console.log(res)
+                    if (res.data.length < 2) {
+                        Proxy
+                            .findByIdAndUpdate(p._id,
+                                {
+                                    '$set': { used: true }
+                                }, (err, od) => {
+                                })
+                    }
+                })
+                .catch(error => {
+                    console.log("error");
 
-        },i*1500)
+                    Proxy
+                        .findByIdAndUpdate(p._id,
+                            {
+                                '$set': { used: true }
+                            }, (err, od) => {
+                            })
+
+                });
+
+        }, i * 2000)
     })
 
 })
